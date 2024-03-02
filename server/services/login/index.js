3de945687e0d480b00b318121d9c690f6c590245
alan <acantero@fi.uba.ar>
2024-03-02 01:34:54 -0300

    login conected to database

--- server/services/login/index.js
+++ server/services/login/index.js
@@ -1,13 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const config = require('./utils/config');
+const logger = require('./utils/logger');
+
 
 const app = express();
 
 app.use(express.json());
 
+
+mongoose.connect(config.MONGODB_URI)
+    .then(() => {
+        logger.info('connected to MongoDB')
+    })
+    .catch((error) => {
+        logger.error('error connecting to MongoDB:', error.message)
+    })
+
 app.use('/',(req, res) => {
     return res.status(200).json({ message: 'Hello from login service' })
 });
 
-app.listen(8002, () => {
+
+const port = process.env.PORT || 8002;
+app.listen(port , () => {
     console.log('Jobs service is running on port 8001');
 });
