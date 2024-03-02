b0fda1399da370d60de2cb35d7490d6e665a5a80
alan <acantero@fi.uba.ar>
2024-03-02 19:22:21 -0300

    login with google working

---
+++ server/services/login/app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('./utils/config');
+const logger = require('./utils/logger');
+const middleware = require('./utils/middleware');
+const loginRouter = require('./routes/login.route');
+const app = express();
+const passport = require('passport');
+const session = require('express-session');
+
+
+
+app.use(express.json());
+
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: 'SECRET' 
+  }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.set('strictQuery', false)
+
+
+mongoose.connect(config.MONGODB_URI)
+    .then(() => {
+        logger.info('connected to MongoDB')
+    })
+    .catch((error) => {
+        logger.error('error connecting to MongoDB:', error.message)
+    })
+
+
+app.use(middleware.requestLogger)
+
+app.use('/login', loginRouter);
+
+module.exports = app;
--- server/services/login/index.js
+++ server/services/login/index.js
@@ -1,28 +1,9 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const config = require('./utils/config');
-const logger = require('./utils/logger');
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const app = require('./app')
 
 
-const app = express();
-
-app.use(express.json());
-
-
-mongoose.connect(config.MONGODB_URI)
-    .then(() => {
-        logger.info('connected to MongoDB')
-    })
-    .catch((error) => {
-        logger.error('error connecting to MongoDB:', error.message)
-    })
-
-app.use('/',(req, res) => {
-    return res.status(200).json({ message: 'Hello from login service' })
-});
-
-
-const port = process.env.PORT || 8002;
-app.listen(port , () => {
-    console.log('Jobs service is running on port 8001');
-});
+const PORT = config.PORT || 8002
+app.listen(PORT, () => {
+    logger.info(`Server running on port ${PORT}`)
+})
