62e3e0fc65c8733c5299dca529de5445517f9717
alan <acantero@fi.uba.ar>
2024-03-03 12:29:16 -0300

    remove prints

--- server/services/login/utils/auth.js
+++ server/services/login/utils/auth.js
@@ -4,8 +4,6 @@ const GoogleStrategy  = require('passport-google-oauth20').Strategy;
 require('dotenv').config()
 
 
-console.log(process.env.GOOGLE_CLIENT_ID)
-
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
